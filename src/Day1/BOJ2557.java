13d2e9fb6f1685327c1d2a4fafcd947b2c0c4f76
hwasun <hwasun.kim2019@gmail.com>
2024-06-10 00:03:39 +0900

    .

--- src/Day1/BOJ2557.java
+++ src/Day1/BOJ2557.java
@@ -3,7 +3,7 @@ package Day1;
 public class BOJ2557 {
     // 메인함수 생성 단축키 psvm
     public static void main(String[] args) {
-        // 출력 단축키 sout 
+        // 출력 단축키 sout  
         System.out.println("Hello, World!");
     }
 }
