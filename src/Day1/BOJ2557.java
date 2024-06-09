af107f09ae7c34b5fcb175cd68fdc27b244539c0
hwasun <hwasun.kim2019@gmail.com>
2024-06-09 23:40:25 +0900

    init commit

---
+++ src/Day1/BOJ2557.java
@@ -0,0 +1,9 @@
+package Day1;
+
+public class BOJ2557 {
+    // 메인함수 생성 단축키 psvm
+    public static void main(String[] args) {
+        // 출력 단축키 sout
+        System.out.println("Hello, World!");
+    }
+}
